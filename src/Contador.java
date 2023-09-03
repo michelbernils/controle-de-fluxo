17576b31c9776c68bca9377cb793e2c0b0e528b6
michelbernils <bernils.michel@gmail.com>
2023-09-03 11:18:54 -0300

    Projeto finalizado

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        // Realizar o for para imprimir os números com base na variável contagem
+        for (int i = parametroUm; i < parametroDois; i++) {
+            String mensagem = String.format("Imprimindo o número %d", i);
+            System.out.println(mensagem);
+        }
+    }
+}
